f8a14b4ef43790a3ef82dc0b9987f95c0c456958
Juan Carlos Espinoza <jcespinoza@unitec.edu>
2013-06-17 09:59:47 -0600

    Initial
    Just Created the project.

---
+++ src/logic/Logic.java
@@ -0,0 +1,20 @@
+package logic;
+
+import java.util.Date;
+
+/**
+ * @author Juan Carlos Espinoza
+ * Class for the calculations.
+ */
+public class Logic {
+    
+    /**
+     * Used for calculating the days after the given date.
+     * @param days
+     * @param start
+     * @return the resulting date with the days included.
+     */
+    public Date calculate(int days, Date start){
+        return null;
+    }
+}
